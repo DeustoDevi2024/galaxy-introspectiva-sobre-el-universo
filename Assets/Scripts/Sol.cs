ad77ad1af5076e7a544fea2cb784ac5459824726
Beñat Ramirez Ortiz <benat.ramirez@gmail.com>
2024-03-10 17:51:03 +0100

    Intentar hacer herencia

---
+++ Assets/Scripts/Sol.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sol : MonoBehaviour
+{
+
+    //public Sol() : base(null, 0, Vector3.zero)
+    //{
+
+    //}
+
+    //// Update is called once per frame
+    //override protected void Update()
+    //{
+        
+    //}
+}
